034de072c3cf5738b027a4ed1475e20c74bca6d8
ShirlineWu <256025004+ShirlineWu@users.noreply.github.com>
2026-04-18 21:40:41 +0800

    增加判斷輸入command後的路徑

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/20260318my_noteproject/',
+export default defineConfig(({command}) => {
+  return{
+  base: command === 'build' ? '/20260318my_noteproject/' : '/',
   plugins: [vue()],
+  }
 })
