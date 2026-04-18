9dd62302857c8b80ca04dda9f7715561d09e2e18
ShirlineWu <256025004+ShirlineWu@users.noreply.github.com>
2026-04-18 21:57:31 +0800

    把輸出的資料夾改成 docs

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig(({command}) => {
   return{
   base: command === 'build' ? '/20260318my_noteproject/' : '/',
   plugins: [vue()],
+
+  build: {
+      outDir: 'docs' // 這是為了讓 GitHub Pages 好讀取而改的輸出資料夾
+    }
   }
 })
