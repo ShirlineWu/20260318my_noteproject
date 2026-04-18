8998b2859f60953ffaf7319a37c969f22d57b68b
ShirlineWu <256025004+ShirlineWu@users.noreply.github.com>
2026-04-18 21:24:27 +0800

    修改vite.config.js repository-name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ShirlineWu/',
+  base: '/20260318my_noteproject/',
   plugins: [vue()],
 })
